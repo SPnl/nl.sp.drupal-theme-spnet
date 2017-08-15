46dd049aedbe2183327d655bc9aaa0ef67cbf9a8
Stijn Dekker <sdekker@sp.nl>
2017-08-15 12:21:19 +0000

    Adds warning on logo for non-production deployments

--- templates/inc/site-header.php
+++ templates/inc/site-header.php
@@ -1,23 +1,18 @@
 <header class="site-header">
     <div class="site-branding">
-        <?php if ($logo): ?>
         <div class="site-logo">
             <a href="<?php print $front_page; ?>" id="logo" rel="home" title="<?php print t('Home'); ?>">
                 <img alt="<?php print t('Home'); ?>" src="<?php print $logo; ?>"/>
             </a>
         </div>
-        <?php endif; ?>
-        <?php if ($site_name): ?>
-        <h1 class="site-name">
-            <a href="<?php print $front_page; ?>" rel="home" title="<?php print t('Home'); ?>"></a>
-        </h1>
+        <?php // Prevent confusion in DTAP streets
+              if(!in_array($_SERVER['SERVER_NAME'], array('www.spnet.nl','spnet.nl'))):  ?>
+                <div class="dev-name"><?php print $_SERVER['SERVER_NAME']; ?></div>
         <?php endif; ?>
     </div>
-    <?php if ($main_menu): ?>
     <nav class="site-navigation">
-        <?php print render(menu_tree('main-menu', 1)); ?>
+      <?php print render(menu_tree('main-menu', 1)); ?>
     </nav>
-    <?php endif; ?>
     <div class="search-toggle">
         <?php if ($page['search']): ?>
         <a class="button btn-search" href="#search">
